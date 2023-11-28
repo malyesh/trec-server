7bb6d87ecb3dae94fef8c05bf8da79eab59e058b
malyesh <74512928+malyesh@users.noreply.github.com>
2023-11-27 20:26:12 -0500

    add unlike functionality

--- controllers/favorites-controller.js
+++ controllers/favorites-controller.js
@@ -40,7 +40,38 @@ const isFavorited = async (req, res) => {
   return res.status(200).json(favorited);
 };
 
+const unFavorite = async (req, res) => {
+  const postId = req.params.postId;
+
+  if (!req.headers.authorization) {
+    return res.status(401).send('Please login');
+  }
+  const authHeader = req.headers.authorization;
+  const authToken = authHeader.split(' ')[1];
+  const decoded = jwt.verify(authToken, process.env.SECRET_KEY);
+
+  try {
+    const rowDeleted = await knex('favorite')
+      .where({ post_id: postId, user_id: decoded.id })
+      .delete();
+
+    if (rowDeleted === 0) {
+      return res
+        .status(404)
+        .json({ message: `Post with ID ${postId} not found` });
+    }
+    return res.status(204).json({
+      message: `Successfully deleted post with ID ${postId}`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to delete post ${error}`,
+    });
+  }
+};
+
 module.exports = {
   favorite,
   isFavorited,
+  unFavorite,
 };
--- routes/favorites-routes.js
+++ routes/favorites-routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const favoritesController = require('../controllers/favorites-controller');
 
 router.route('/').post(favoritesController.favorite);
-router.route('/:postId').get(favoritesController.isFavorited);
+
+router
+  .route('/:postId')
+  .get(favoritesController.isFavorited)
+  .delete(favoritesController.unFavorite);
 
 module.exports = router;
